42794e02c2eb3b6a1b51ec6c16ca11987b4561a2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2019-12-13 16:10:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ scripts=[
           "edit":"https://github.com/ktsuttlemyre/RogueBookmarklets/edit/master/{{ marklet.path | escape }}",
           "href":"javascript:{{ marklet.path | escape }}",
           "name":"{{ marklet.name | escape }}",
-          "src":"https://raw.githubusercontent.com/ktsuttlemyre/RogueBookmarklets/master/bookmarklets/{{ marklet.name | escape }}",
+          "src":"https://cdn.jsdelivr.net/gh/ktsuttlemyre/RogueBookmarklets/{{ marklet.path | escape }}",
+          "github_raw":"https://raw.githubusercontent.com/ktsuttlemyre/RogueBookmarklets/master/bookmarklets/{{ marklet.name | escape }}",
+          "github_pages":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | escape }}"
         },
       {% endif %}
     {% endfor %}
