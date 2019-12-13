9ae35e44cfa79a7d7ddce4246c6e33ba46ef45e6
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2019-12-13 18:34:05 -0500

    Update RogueRunner_src.js

--- RogueRunner_src.js
+++ RogueRunner_src.js
@@ -1,268 +1,267 @@
-
-    var RogueBookmarks=(function(){
-appendToHead = function(el) {
-    document.getElementsByTagName('head')[0].appendChild(el);
-}
-
-var script = document.createElement('script');
-script.setAttribute('src', 'https://ktsuttlemyre.github.io/RogueBookmarklets/index.js');
-script.setAttribute('type', 'text/javascript');
-script.setAttribute('crossorigin', "anonymous")
-appendToHead(script);
-
-//https://codebeautify.org/string-builder
-var cssText = '   /* The Modal (background) */  ' +
-    '   .modal {  ' +
-    '     display: none; /* Hidden by default */  ' +
-    '     position: fixed; /* Stay in place */  ' +
-    '     z-index: 1; /* Sit on top */  ' +
-    '     left: 0;  ' +
-    '     top: 0;  ' +
-    '     width: 100%; /* Full width */  ' +
-    '     height: 100%; /* Full height */  ' +
-    '     overflow: auto; /* Enable scroll if needed */  ' +
-    '     background-color: rgb(0,0,0); /* Fallback color */  ' +
-    '     background-color: rgba(0,0,0,0.4); /* Black w/ opacity */  ' +
-    '   }  ' +
-    '     ' +
-    '   /* Modal Content/Box */  ' +
-    '   .modal-content {  ' +
-    '     background-color: #fefefe;  ' +
-    '     margin: 15% auto; /* 15% from the top and centered */  ' +
-    '     padding: 20px;  ' +
-    '     border: 1px solid #888;  ' +
-    '     width: 80%; /* Could be more or less, depending on screen size */  ' +
-    '   }  ' +
-    '     ' +
-    '   /* The Close Button */  ' +
-    '   .close {  ' +
-    '     color: #aaa;  ' +
-    '     float: right;  ' +
-    '     font-size: 28px;  ' +
-    '     font-weight: bold;  ' +
-    '   }  ' +
-    '     ' +
-    '   .close:hover,  ' +
-    '   .close:focus {  ' +
-    '     color: black;  ' +
-    '     text-decoration: none;  ' +
-    '     cursor: pointer;  ' +
-    '   }  ' +
-     '   #RogueRunner{  '  + 
- '     position: relative;  '  + 
- '   }  '  + 
- '     '  + 
- '   #RogueRunner > #input {  '  + 
- '     font-size: 1em;  '  + 
- '     height: 1.5em;  '  + 
- '     width: 20em;  '  + 
- '     '  + 
- '   -webkit-border-radius: 15px;  '  + 
- '   -moz-border-radius: 15px;  '  + 
- '   border-radius: 15px;  '  + 
- '   padding-bottom: 1em;  '  + 
- '   padding-left: 0.5em;  '  + 
- '   padding-right: 0.5em;  '  + 
- '   }  '  + 
- '     '  + 
- '   #RogueRunner > .placeholder {  '  + 
- '     position: absolute;  '  + 
- '     font-size: 25px;  '  + 
- '     pointer-events: none;  '  + 
- '     left: 1em;  '  + 
- '     bottom: 1px;  '  + 
- '     transition: 0.1s ease all;  '  + 
- '   }  '  + 
- '     '  + 
- '    #RogueRunner #input:focus ~ .placeholder{  '  + 
- '     bottom: 1px;  '  + 
- '     font-size: 13px;  '  + 
- '   }  '  + 
- '     '  + 
- '    ';
-
-var css = document.createElement("style");
-css.type = "text/css";
-if ("textContent" in css)
-    css.textContent = cssText;
-else
-    css.innerText = cssText;
-appendToHead(css);
-
-//https://github.com/ded/domready
-! function(e, t) {
-    typeof module != "undefined" ? module.exports = t() : typeof define == "function" && typeof define.amd == "object" ? define(t) : this[e] = t()
-}("domready", function() {
-    var e = [],
-        t, n = typeof document == "object" && document,
-        r = n && n.documentElement.doScroll,
-        i = "DOMContentLoaded",
-        s = n && (r ? /^loaded|^c/ : /^loaded|^i|^c/).test(n.readyState);
-    return !s && n && n.addEventListener(i, t = function() {
-            n.removeEventListener(i, t), s = 1;
-            while (t = e.shift()) t()
-        }),
-        function(t) {
-            s ? setTimeout(t, 0) : e.push(t)
-        }
-})
-
-
-// Create element; can be whatever you want, e. g. div, h1, p, img...
-var div = document.createElement('div');
-div.id = 'myModal';
-div.className = 'modal';
-
-var innerDiv = document.createElement('div');
-innerDiv.className = 'modal-content';
-
-var closeButton = document.createElement('span')
-closeButton.classname = 'close';
-closeButton.innerHTML = '&times;';
-
-var paragraph = document.createElement('p')
-
-
-
-var wrapper= document.createElement('div')
-wrapper.id='RogueRunner'
-
-var input = document.createElement('input')
-input.id = "input"
-input.type = 'text'
-input.onkeyup = "RogueBookmarks.changeInput(this.value)
-
-var span = document.createElement('span');
-span.id='result'
-span.className='placeholder'
-
-wrapper.appendChild(input)
-wrapper.appendChild(span)
-
-// Append the div to the body
-paragraph.appendChild(wrapper);
-innerDiv.appendChild(paragraph);
-innerDiv.appendChild(closeButton);
-div.appendChild(innerDiv);
-domready(function() {
-    document.body.appendChild(div);
-});
-
-initSearch = function() {
-    if ( !window.scripts) {
-        return setTimeout(initSearch, 0);
+var RogueBookmarks = (function() {
+    appendToHead = function(el) {
+        document.getElementsByTagName('head')[0].appendChild(el);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    horsey(document.querySelector('#search_bar'), {
-        source: [{
-            list: scripts
-        }],
-        getText: 'name',
-        getValue: 'src',
-        predictNextSearch: function(info) {
-            console.log('loading',info)
-            var script = document.createElement('script');
-            script.setAttribute('src', info.selection.src);
-            script.setAttribute('type', 'text/javascript');
-            script.setAttribute('crossorigin', "anonymous");
-            appendToHead(script);
-        }
+
+    var script = document.createElement('script');
+    script.setAttribute('src', 'https://ktsuttlemyre.github.io/RogueBookmarklets/index.js');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('crossorigin', "anonymous")
+    appendToHead(script);
+
+    //https://codebeautify.org/string-builder
+    var cssText = '   /* The Modal (background) */  ' +
+        '   .modal {  ' +
+        '     display: none; /* Hidden by default */  ' +
+        '     position: fixed; /* Stay in place */  ' +
+        '     z-index: 1; /* Sit on top */  ' +
+        '     left: 0;  ' +
+        '     top: 0;  ' +
+        '     width: 100%; /* Full width */  ' +
+        '     height: 100%; /* Full height */  ' +
+        '     overflow: auto; /* Enable scroll if needed */  ' +
+        '     background-color: rgb(0,0,0); /* Fallback color */  ' +
+        '     background-color: rgba(0,0,0,0.4); /* Black w/ opacity */  ' +
+        '   }  ' +
+        '     ' +
+        '   /* Modal Content/Box */  ' +
+        '   .modal-content {  ' +
+        '     background-color: #fefefe;  ' +
+        '     margin: 15% auto; /* 15% from the top and centered */  ' +
+        '     padding: 20px;  ' +
+        '     border: 1px solid #888;  ' +
+        '     width: 80%; /* Could be more or less, depending on screen size */  ' +
+        '   }  ' +
+        '     ' +
+        '   /* The Close Button */  ' +
+        '   .close {  ' +
+        '     color: #aaa;  ' +
+        '     float: right;  ' +
+        '     font-size: 28px;  ' +
+        '     font-weight: bold;  ' +
+        '   }  ' +
+        '     ' +
+        '   .close:hover,  ' +
+        '   .close:focus {  ' +
+        '     color: black;  ' +
+        '     text-decoration: none;  ' +
+        '     cursor: pointer;  ' +
+        '   }  ' +
+        '   #RogueRunner{  ' +
+        '     position: relative;  ' +
+        '   }  ' +
+        '     ' +
+        '   #RogueRunner > #input {  ' +
+        '     font-size: 1em;  ' +
+        '     height: 1.5em;  ' +
+        '     width: 20em;  ' +
+        '     ' +
+        '   -webkit-border-radius: 15px;  ' +
+        '   -moz-border-radius: 15px;  ' +
+        '   border-radius: 15px;  ' +
+        '   padding-bottom: 1em;  ' +
+        '   padding-left: 0.5em;  ' +
+        '   padding-right: 0.5em;  ' +
+        '   }  ' +
+        '     ' +
+        '   #RogueRunner > .placeholder {  ' +
+        '     position: absolute;  ' +
+        '     font-size: 25px;  ' +
+        '     pointer-events: none;  ' +
+        '     left: 1em;  ' +
+        '     bottom: 1px;  ' +
+        '     transition: 0.1s ease all;  ' +
+        '   }  ' +
+        '     ' +
+        '    #RogueRunner #input:focus ~ .placeholder{  ' +
+        '     bottom: 1px;  ' +
+        '     font-size: 13px;  ' +
+        '   }  ' +
+        '     ' +
+        '    ';
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    if ("textContent" in css)
+        css.textContent = cssText;
+    else
+        css.innerText = cssText;
+    appendToHead(css);
+
+    //https://github.com/ded/domready
+    ! function(e, t) {
+        typeof module != "undefined" ? module.exports = t() : typeof define == "function" && typeof define.amd == "object" ? define(t) : this[e] = t()
+    }("domready", function() {
+        var e = [],
+            t, n = typeof document == "object" && document,
+            r = n && n.documentElement.doScroll,
+            i = "DOMContentLoaded",
+            s = n && (r ? /^loaded|^c/ : /^loaded|^i|^c/).test(n.readyState);
+        return !s && n && n.addEventListener(i, t = function() {
+                n.removeEventListener(i, t), s = 1;
+                while (t = e.shift()) t()
+            }),
+            function(t) {
+                s ? setTimeout(t, 0) : e.push(t)
+            }
+    })
+
+
+    // Create element; can be whatever you want, e. g. div, h1, p, img...
+    var div = document.createElement('div');
+    div.id = 'myModal';
+    div.className = 'modal';
+
+    var innerDiv = document.createElement('div');
+    innerDiv.className = 'modal-content';
+
+    var closeButton = document.createElement('span')
+    closeButton.classname = 'close';
+    closeButton.innerHTML = '&times;';
+
+    var paragraph = document.createElement('p')
+
+
+
+    var wrapper = document.createElement('div')
+    wrapper.id = 'RogueRunner'
+
+    var input = document.createElement('input')
+    input.id = "input"
+    input.type = 'text'
+    input.onkeyup = "RogueBookmarks.changeInput(this.value)"
+
+    var span = document.createElement('span');
+    span.id = 'result'
+    span.className = 'placeholder'
+
+    wrapper.appendChild(input)
+    wrapper.appendChild(span)
+
+    // Append the div to the body
+    paragraph.appendChild(wrapper);
+    innerDiv.appendChild(paragraph);
+    innerDiv.appendChild(closeButton);
+    div.appendChild(innerDiv);
+    domready(function() {
+        document.body.appendChild(div);
     });
-    
-    
-    
 
-var people = ['Steven', 'Sean', 'Stefan', 'Sam', 'Nathan'];
+    initSearch = function() {
+        if (!window.scripts) {
+            return setTimeout(initSearch, 0);
+        }
 
-function matchPeople(input) {
-  var reg = new RegExp(input.split('').join('\\w*').replace(/\W/, ""), 'i');
-  var list=[]
-  for(var i=0;i<people.length;i++){
-  	var person = people[i]
-    if (person.match(reg)) {
-      list.push('<a href="#" onFocus="RogueBookmarks.setSelection(\''+person+'\')">'+person+"</a>");
-		}
-  }
-  return list;
-}
-var previousCache=""
-function changeInput(val) {
-  var autoCompleteResult = matchPeople(val);
-  
-  var current = autoCompleteResult.toString()
-  if(previousCache!=current){
-  	document.getElementById("result").innerHTML = autoCompleteResult;
-    previousCache=current
-  }
-  
-  
-  document.onkeydown = function(evt) {
-      var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
-      if (keyCode == 13) {
-        //if (event.target == modal) {
-        //  modal.style.display = "none";
-           if (document.activeElement){
-        		selection=document.activeElement.text
+
+
+
+        horsey(document.querySelector('#search_bar'), {
+            source: [{
+                list: scripts
+            }],
+            getText: 'name',
+            getValue: 'src',
+            predictNextSearch: function(info) {
+                console.log('loading', info)
+                var script = document.createElement('script');
+                script.setAttribute('src', info.selection.src);
+                script.setAttribute('type', 'text/javascript');
+                script.setAttribute('crossorigin', "anonymous");
+                appendToHead(script);
             }
-        		alert(selection)
-      	//}
-    	}
-    }
-}
-var selection='';
-function setSelection(slection){
-	selection=slection;
-}
+        });
 
 
-    
-    
-    
-    document.getElementById("search_bar").focus();
-}
 
-//https://www.w3schools.com/howto/howto_css_modals.asp
-// Get the modal
-var modal = div // document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-var span = closeButton; //document.getElementsByClassName("close")[0];
+        var people = ['Steven', 'Sean', 'Stefan', 'Sam', 'Nathan'];
 
-// When the user clicks on the button, open the modal
-//btn.onclick = function() {
-domready(function() {
-    modal.style.display = "block";
+        function matchPeople(input) {
+            var reg = new RegExp(input.split('').join('\\w*').replace(/\W/, ""), 'i');
+            var list = []
+            for (var i = 0; i < people.length; i++) {
+                var person = people[i]
+                if (person.match(reg)) {
+                    list.push('<a href="#" onFocus="RogueBookmarks.setSelection(\'' + person + '\')">' + person + "</a>");
+                }
+            }
+            return list;
+        }
+        var previousCache = ""
 
-    initSearch(input)
-    document.onkeydown = function(evt) {
-        var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
-        if (keyCode == 27) {
-            if (event.target == modal) {
-                modal.style.display = "none";
+        function changeInput(val) {
+            var autoCompleteResult = matchPeople(val);
+
+            var current = autoCompleteResult.toString()
+            if (previousCache != current) {
+                document.getElementById("result").innerHTML = autoCompleteResult;
+                previousCache = current
+            }
+
+
+            document.onkeydown = function(evt) {
+                var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+                if (keyCode == 13) {
+                    //if (event.target == modal) {
+                    //  modal.style.display = "none";
+                    if (document.activeElement) {
+                        selection = document.activeElement.text
+                    }
+                    alert(selection)
+                    //}
+                }
             }
         }
+        var selection = '';
+
+        function setSelection(slection) {
+            selection = slection;
+        }
+
+
+
+
+        document.getElementById("search_bar").focus();
     }
-})
-//}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+    //https://www.w3schools.com/howto/howto_css_modals.asp
+    // Get the modal
+    var modal = div // document.getElementById("myModal");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // Get the <span> element that closes the modal
+    var span = closeButton; //document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    //btn.onclick = function() {
+    domready(function() {
+        modal.style.display = "block";
+
+        initSearch(input)
+        document.onkeydown = function(evt) {
+            var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+            if (keyCode == 27) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+    })
+    //}
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
-        return {changeInput:changeInput,setSelection:setSelection}
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    return {
+        changeInput: changeInput,
+        setSelection: setSelection
+    }
 
 })()
