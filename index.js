7b6c0180facd35a7f4539ecfa5539506b43dcb2b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2019-12-14 22:27:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ scripts={
     {% for marklet in site.static_files %}
       {% if marklet.path contains 'bookmarklets/' %}
     "{{ marklet.basename | escape }}":{
-          "edit":"https://github.com/ktsuttlemyre/RogueBookmarklets/edit/master/{{ marklet.path | escape }}",
-          "href":"javascript:{{ marklet.path | escape }}",
-          "name":"{{ marklet.name | escape }}",
-          "src":"https://cdn.jsdelivr.net/gh/ktsuttlemyre/RogueBookmarklets/{{ marklet.path | escape }}",
-          "github_raw":"https://raw.githubusercontent.com/ktsuttlemyre/RogueBookmarklets/master/bookmarklets/{{ marklet.name | escape }}",
-          "github_pages":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | escape }}"
+          "edit":"https://github.com/ktsuttlemyre/RogueBookmarklets/edit/master/{{ marklet.path | url_escape }}",
+          "href":"javascript:{{ marklet.path | url_escape }}",
+          "name":"{{ marklet.name }}",
+          "src":"https://cdn.jsdelivr.net/gh/ktsuttlemyre/RogueBookmarklets/{{ marklet.path | url_escape }}",
+          "github_raw":"https://raw.githubusercontent.com/ktsuttlemyre/RogueBookmarklets/master/bookmarklets/{{ marklet.name | url_escape }}",
+          "github_pages":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | url_escape }}"
         },
       {% endif %}
     {% endfor %}
