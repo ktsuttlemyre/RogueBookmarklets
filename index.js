fa2cf7a2458e2fcd8c982998ded8bf3f6f03bad3
Kyle S <kyles@Kyles-MacBook-Pro.local>
2019-12-16 00:52:10 -0500

    yee

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ scripts={
       {% if marklet.path contains 'bookmarklets/' %}
     "{{ marklet.basename | escape }}":{
           "basename":"{{ marklet.basename }}",
-          "name":"{{ marklet.name }}",
           "path":"{{ marklet.path }}",
           "modified_time":"{{ marklet.modified_time }}",
-          "extname":"{{ marklet.extname }}",
           "edit":"https://github.com/ktsuttlemyre/RogueBookmarklets/edit/master/{{ marklet.path | url_escape }}",
           "href":"javascript:{{ marklet.path | url_escape }}",
-          "src":"https://cdn.jsdelivr.net/gh/ktsuttlemyre/RogueBookmarklets/{{ marklet.path | url_escape }}",
+          "src":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | url_escape }}",
           "github_raw":"https://raw.githubusercontent.com/ktsuttlemyre/RogueBookmarklets/master/bookmarklets/{{ marklet.name | url_escape }}",
-          "github_pages":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | url_escape }}"
+          "github_pages":"https://ktsuttlemyre.github.io/RogueBookmarklets/{{ marklet.path | url_escape }}",
+          "jsdelivr":"https://cdn.jsdelivr.net/gh/ktsuttlemyre/RogueBookmarklets/{{ marklet.path | url_escape }}",
         },
       {% endif %}
     {% endfor %}
