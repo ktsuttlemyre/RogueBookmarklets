94fd13468738fab47e7898333723b5cded2715f0
Kyle S <kyles@Kyles-MacBook-Pro.local>
2019-12-21 12:43:58 -0500

    fixing local storage for inject script

--- RogueRunner_inject_src.js
+++ RogueRunner_inject_src.js
@@ -31,7 +31,6 @@
  * @constructor
  */
 	var CrossOriginLocalStorage = function(currentWindow, iframe, allowedOrigins, onMessage) {
-		this.allowedOrigins = allowedOrigins;
 
 		var childWindow;
 		// some browser (don't remember which one) throw exception when you try to access
@@ -42,8 +41,8 @@
 			childWindow = iframe.contentWindow;
 		}
 
-		this._listener = function (event) {
-			if (!this.isAllowedOrigin(event.origin)) {
+		var _listener=this._listener = function (event) {
+			if (!isAllowedOrigin(event.origin)) {
 				return;
 			}
 
@@ -52,34 +51,34 @@
 
 		// Support IE8 with attachEvent
 		if (currentWindow.addEventListener) {
-			currentWindow.addEventListener('message', this._listener, false);
+			currentWindow.addEventListener('message', _listener, false);
 		} else {
-			currentWindow.attachEvent('onmessage', this._listener);
+			currentWindow.attachEvent('onmessage', _listener);
 		}
 
 
-		this.isAllowedOrigin = function (origin) {
-			return this.allowedOrigins.includes(origin);
+		var isAllowedOrigin = function (origin) {
+			return allowedOrigins.includes(origin);
 		}
 
-		this.getData = function (key) {
+		var getData=this.getData = function (key) {
 			messageData = {
 				key: key,
 				method: 'get',
 			}
-			this.postMessage(messageData);
+			postMessage(messageData);
 		}
 
-		this.setData = function(key, data) {
+		var setData = this.setData = function(key, data) {
 			messageData = {
 				key: key,
 				method: 'set',
 				data: data,
 			}
-			this.postMessage(messageData);
+			postMessage(messageData);
 		}
 
-		this.postMessage = function(messageData) {
+		var postMessage=this.postMessage = function(messageData) {
 			childWindow.postMessage(JSON.stringify(messageData), '*');
 		}
 	};
@@ -102,10 +101,9 @@
 				console.error('Unknown method "' + payload.method + '"', payload);
 			}
 		};
-		var cross = new CrossOriginLocalStorage(window, iframe, allowedOrigins, onMessage);
+		var cross = self['RogueBookmarklets']['cross'] = new CrossOriginLocalStorage(self, iframe, allowedOrigins, onMessage);
 		cross.setData('name', 'buren')
-		cross.getData('name')
-		RogueBookmarklets.cross=cross;
+		alert(cross.getData('name'))
 	});
 
 	self['RogueBookmarklets']=self['RogueBookmarklets'] || {} //in block notation so closure compiler will 'export' the vairable
