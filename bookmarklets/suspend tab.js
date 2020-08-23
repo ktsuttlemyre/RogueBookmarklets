98ed8dae8f622c978574038d66efafef67ac5319
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-08-23 02:37:08 -0400

    Update suspend tab.js

--- bookmarklets/suspend tab.js
+++ bookmarklets/suspend tab.js
@@ -1,7 +1,7 @@
 var storageURL="https://ktsuttlemyre.github.io/RogueBookmarklets/SuspendTab.html#"
 
 if(window.location.href.indexOf(storageURL) == 0){
-	window.location=Array.prototype.slice.call(window.location.href.split('#'), 1).join('#');
+	window['SuspendTab']['unsuspend']();
 }else{
-	window.location=storageURL+window.location
+	window.location=storageURL+'#?l='+window.location
 }
