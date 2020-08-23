4c2c6bb9287b9029f7d6ad143fea7c11940be5a1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-08-23 04:27:23 -0400

    Create html2canvas.js

---
+++ bookmarklets/html2canvas.js
@@ -0,0 +1,8 @@
+window.RogueBM.loadScript('https://html2canvas.hertzen.com/dist/html2canvas.js','javascript',function(err){
+  if(e){
+  console.error(e)
+  }
+  html2canvas(document.body).then(function(canvas) {
+    document.body.appendChild(canvas);
+  });
+  })
