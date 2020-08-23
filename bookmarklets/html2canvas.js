1237b94f82bcffe2815ad18409a54681a545c7f9
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-08-23 04:55:38 -0400

    Update html2canvas.js

--- bookmarklets/html2canvas.js
+++ bookmarklets/html2canvas.js
@@ -1,6 +1,7 @@
 window.RogueBM.loadScript('https://html2canvas.hertzen.com/dist/html2canvas.js','javascript',function(err){
-  if(e){
-  console.error(e)
+  if(err){
+    console.error(err)
+    return
   }
   html2canvas(document.body).then(function(canvas) {
     document.body.appendChild(canvas);
