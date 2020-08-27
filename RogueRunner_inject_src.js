1cef11d7548b476b0f377187993f21d00aeca0a0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-08-27 03:01:46 -0400

    Update RogueRunner_inject_src.js

--- RogueRunner_inject_src.js
+++ RogueRunner_inject_src.js
@@ -14,237 +14,248 @@
 
 
 (function (vers,self,options,cmd) {
-	function UUID(){
-		return Math.floor(Math.random()*9000000000) + 1000000000+'-'+Date.now();
-	}
+    function UUID(){
+        return Math.floor(Math.random()*9000000000) + 1000000000+'-'+Date.now();
+    }
 
 
-	// show err
-	function showError(message){
-		//statusBar.innerHTML=message
-		var args = Array.prototype.slice.call( arguments );
-		args.unshift("RogueBM[injection]: ");
-		console.error.apply(console, args);
-	}
+    // show err
+    function showError(message){
+        //statusBar.innerHTML=message
+        var args = Array.prototype.slice.call( arguments );
+        args.unshift("RogueBM[injection]: ");
+        console.error.apply(console, args);
+    }
 
-	function appendToHead(el,callback) {
-		document.getElementsByTagName('head')[0].appendChild(el);
-	}
+    function appendToHead(el,callback) {
+        document.getElementsByTagName('head')[0].appendChild(el);
+    }
 
-	function ScriptOBJ(src,code,err) { //callback might not work
-		var script = document.createElement('script');
-		script.setAttribute('type', 'text/javascript');
-		if(src){
-			script.setAttribute('src',src);
-			script.setAttribute('crossorigin', "anonymous");
-			script.onerror = err;
-		}else{
-			try {
-				script.appendChild(document.createTextNode(code));
-			} catch (e) { //silent error fallback for shitty browsers
-				script.text = code;
-			}
-		}
-		return script;
-	}
+    function ScriptOBJ(src,code,err) { //callback might not work
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        if(src){
+            script.setAttribute('src',src);
+            script.setAttribute('crossorigin', "anonymous");
+            script.onerror = err;
+        }else{
+            try {
+                script.appendChild(document.createTextNode(code));
+            } catch (e) { //silent error fallback for shitty browsers
+                script.text = code;
+            }
+        }
+        return script;
+    }
 
-	function getScriptFromLocalStorageIframe(url,err){
-		//start the injection
-		var xDLStorage=self['RogueBM']['xDLStorage'];
-		if(!xDLStorage){
-			showError('Error injecting '+url,' xDLStorage isn\'t loaded as a backup either',err);
-		}
+    function getScriptFromLocalStorageIframe(url,err){
+        //start the injection
+        var xDLStorage=self['RogueBM']['xDLStorage'];
+        if(!xDLStorage){
+            showError('Error injecting '+url,' xDLStorage isn\'t loaded as a backup either',err);
+        }
 
-		xDLStorage['getScript'](url,function(payload){
-			payload.error && showError("Error loading script from xDLStorage",payload.error);
-			try{
-				appendToHead(ScriptOBJ(null,payload.data));
-			}catch(e){
-				eval(payload);
-			}
-		});
-	}
+        xDLStorage['getScript'](url,function(payload){
+          if(payload.error){
+            showError("Error loading script from xDLStorage",payload.error);
+            }
+            try{
+                appendToHead(ScriptOBJ(null,payload.data));
+            }catch(e){
+                eval(payload);
+            }
+        });
+    }
 
 
-	function loadCrossOriginLocalStorage(){
-		//  Use this micro framework to see if the dom is ready
-		//some modifications to make it init faster
-		//https://github.com/ded/domready
-		var domready = (function() {
-			var e = [],
-				t, n = typeof document == "object" && document,
-				r = n && n.documentElement.doScroll,
-				i = "DOMContentLoaded",
-				s = n && (r ? /^loaded|^c/ : /^loaded|^i|^c/).test(n.readyState);
-			return !s && n && n.addEventListener(i, t = function() {
-					n.removeEventListener(i, t), s = 1;
-					while (t = e.shift()) t();
-				}),
-				function(t) {
-					s ? setTimeout(t, 0) : e.push(t);
-				};
-		})();
+    function loadCrossOriginLocalStorage(){
+        //  Use this micro framework to see if the dom is ready
+        //some modifications to make it init faster
+        //https://github.com/ded/domready
+        var domready = (function() {
+            var e = [],
+                t, n = typeof document == "object" && document,
+                r = n && n.documentElement.doScroll,
+                i = "DOMContentLoaded",
+                s = n && (r ? /^loaded|^c/ : /^loaded|^i|^c/).test(n.readyState);
+            return !s && n && n.addEventListener(i, t = function() {
+                    n.removeEventListener(i, t);
+              			s = 1;
+              while (t = e.shift()){
+                t();
+              }
+                }),
+                function(t) {
+              	if(s){
+                	setTimeout(t, 0);
+                }else{
+                  e.push(t);
+                }
+                };
+        })();
 
-		/**
-		 * @constructor
-		 */
-		var CrossOriginLocalStorage = function(currentWindow, url, allowedOrigins) {
-			var xOriginElement; //could be an iframe or a window
-			var preloadQueue=[];
-			var doPreloadHandlers = function(){
-				if(!preloadQueue){
-					return;
-				}
-				for(var i=0;i<preloadQueue.length;i++){
-					xOriginElement.postMessage(JSON.stringify(preloadQueue[i]), '*'); //TODO fix this security risk
-				}
-				preloadQueue=null;
-			};
+        /**
+         * @constructor
+         */
+        var CrossOriginLocalStorage = function(currentWindow, url, allowedOrigins) {
+            var xOriginElement; //could be an iframe or a window
+            var preloadQueue=[];
+            var doPreloadHandlers = function(){
+                if(!preloadQueue){
+                    return;
+                }
+                for(var i=0;i<preloadQueue.length;i++){
+                    xOriginElement.postMessage(JSON.stringify(preloadQueue[i]), '*'); //TODO fix this security risk
+                }
+                preloadQueue=null;
+            };
 
-			// var url,iframe;
-			// if(typeof url == 'string'){
-			// 	url=url
-			// }else{
-			// 	iframe=url
-			// }
+            // var url,iframe;
+            // if(typeof url == 'string'){
+            //  url=url
+            // }else{
+            //  iframe=url
+            // }
 
-			domready(function(){
-				var forcePopOut=false;
+            domready(function(){
+                var forcePopOut=false;
         var iframe;
-				if(!forcePopOut){
-					iframe = document.createElement('iframe');
-					iframe.addEventListener("load",doPreloadHandlers);
-					iframe.onerror=function(){alert('yeeeet');};
-					iframe.src = url;
-					iframe.style.display = "none";
-					iframe.style.position = 'absolute'; //ensure no reflow
-					document.body.appendChild(iframe);
+                if(!forcePopOut){
+                    iframe = document.createElement('iframe');
+                    iframe.addEventListener("load",doPreloadHandlers);
+                    iframe.onerror=function(){alert('yeeeet');};
+                    iframe.src = url;
+                    iframe.style.display = "none";
+                    iframe.style.position = 'absolute'; //ensure no reflow
+                    document.body.appendChild(iframe);
 
-					//some browser (don't remember which one) throw exception when you try to access
-					//contentWindow for the first time, it works when you do that second time
-					try {
-						xOriginElement = iframe.contentWindow;
-					} catch(e) { //silent error, fallback for browsers
-						xOriginElement = iframe.contentWindow;
-					}
-				}
-				//TODO
-				//use a webworker?
-				//https://stackoverflow.com/questions/20410119/cross-domain-web-worker
-				//use a socket?
-				//use XMLHttpRequest?
+                    //some browser (don't remember which one) throw exception when you try to access
+                    //contentWindow for the first time, it works when you do that second time
+                    try {
+                        xOriginElement = iframe.contentWindow;
+                    } catch(e) { //silent error, fallback for browsers
+                        xOriginElement = iframe.contentWindow;
+                    }
+                }
+                //TODO
+                //use a webworker?
+                //https://stackoverflow.com/questions/20410119/cross-domain-web-worker
+                //use a socket?
+                //use XMLHttpRequest?
 
-				//if the iframe fails use a window
-				if(!iframe || !iframe.contentDocument || !xOriginElement){
-					xOriginElement = window.open(url.src || url, 'RogueRunner', 'scrollbars=no, width=1, height=1, top=1, left=1');
-					//xOriginElement[xOriginElement.addEventListener ? 'addEventListener' : 'attachEvent'](
-					//(xOriginElement.attachEvent ? 'on' : '') + 'load', doPreloadHandlers, false)
-					xOriginElement.blur();
-				}
-			});
+                //if the iframe fails use a window
+                if(!iframe || !iframe.contentDocument || !xOriginElement){
+                    xOriginElement = window.open(url.src || url, 'RogueRunner', 'scrollbars=no, width=1, height=1, top=1, left=1');
+                    //xOriginElement[xOriginElement.addEventListener ? 'addEventListener' : 'attachEvent'](
+                    //(xOriginElement.attachEvent ? 'on' : '') + 'load', doPreloadHandlers, false)
+                    xOriginElement.blur();
+                }
+            });
 
-			var messageQueue={};
-			var isAllowedOrigin = function (origin) {
-				return allowedOrigins.includes(origin);
-			};
-			var _listener = function (event) {
-				console.log('got message',event);
-				if (!isAllowedOrigin(event.origin)) {
-					console.warn('rejected post message from',event.origin,'Allowed origins are',allowedOrigins, 'you attempted', event);
-					return;
-				}
+            var messageQueue={};
+            var isAllowedOrigin = function (origin) {
+                return allowedOrigins.includes(origin);
+            };
+            var _listener = function (event) {
+                console.log('got message',event);
+                if (!isAllowedOrigin(event.origin)) {
+                    console.warn('rejected post message from',event.origin,'Allowed origins are',allowedOrigins, 'you attempted', event);
+                    return;
+                }
 
-				var data=JSON.parse(event.data);
-				if(data.error){
-					showError(data.error,event);
-				}
-				//debugger
-				if(data.ready){
-					console.log('doing preload handlers');
-					doPreloadHandlers();
-					return;
-				}
+                var data=JSON.parse(event.data);
+                if(data.error){
+                    showError(data.error,event);
+                }
+                //debugger
+                if(data.ready){
+                    console.log('doing preload handlers');
+                    doPreloadHandlers();
+                    return;
+                }
 
-				if(data['messageID']==null){
-					console.error('need data.messageID for callbacks to function',event);
-					return;
-				}
-
-
-				var handler=messageQueue[data['messageID']];
-				if(handler){
-					if(handler.method!=handler.method){
-						showError('methods do not match. Possible security risk');
-						return;
-					}
-					handler.fn && handler.fn(data, event);
-					messageQueue[data.messageID]=null;
-					delete messageQueue[data.messageID];
-				}else{
-					showError('no handler found for ',event['messageID'],event);
-				}
-			};
-
-			// Support IE8 with attachEvent
-			if (currentWindow.addEventListener) {
-				currentWindow.addEventListener('message', _listener, false);
-			} else {
-				currentWindow.attachEvent('onmessage', _listener);
-			}
-
-			this['getScript'] = function(url,handler){
-				var messageData = {
-					method: 'getScript',
-					url:url,
-				};
-				this.postMessage(messageData,handler);
-			};
-
-			this['postMessage'] = function(messageData,handler) {
-				// var str=new Array(17);
-				// for(var i=0;i<15;i++){
-				// 	var r = Math.random() * 16 | 0;
-				// 	//v = c == 'x' ? r : (r & 0x3 | 0x8);
-				// 	str[i]=r.toString(16);
-				// }
-				// str[15]='-'+Date.now() //put the date on the end to speed up
-				//var id=str.join('')
-
-				var id=UUID();
-
-				messageData['messageID']=id;
-				messageQueue[id]={fn:handler,method:messageData.method};
-				if(preloadQueue != null){
-					preloadQueue.push(messageData);
-					return;
-				}
-				xOriginElement.postMessage(JSON.stringify(messageData), '*'); //TODO fix this security risk
-			};
-		};
-		self['RogueBM']['CrossOriginLocalStorage']= CrossOriginLocalStorage;
-
-		var allowedOrigins = ['https://ktsuttlemyre.github.io'];
-		self['RogueBM']['xDLStorage'] = new CrossOriginLocalStorage(self, 'https://ktsuttlemyre.github.io/RogueBookmarklets/RogueRunner.html' , allowedOrigins);
-	}
-
-	
+                if(data['messageID']==null){
+                    console.error('need data.messageID for callbacks to function',event);
+                    return;
+                }
 
 
+                var handler=messageQueue[data['messageID']];
+                if(handler){
+                    if(handler.method!=handler.method){
+                        showError('methods do not match. Possible security risk');
+                        return;
+                    }
+                    if(handler.fn){
+                      handler.fn(data, event);
+                    }
+                    messageQueue[data.messageID]=null;
+                    delete messageQueue[data.messageID];
+                }else{
+                    showError('no handler found for ',event['messageID'],event);
+                }
+            };
 
-	var NotLoadedRogueBM=!self['RogueBM'];
+            // Support IE8 with attachEvent
+            if (currentWindow.addEventListener) {
+                currentWindow.addEventListener('message', _listener, false);
+            } else {
+                currentWindow.attachEvent('onmessage', _listener);
+            }
 
-	// pollyfill for date.now
-	if (!Date.now) {
-		Date.now = function now() {
-			return new Date().getTime();
-		};
-	}
+            this['getScript'] = function(url,handler){
+                var messageData = {
+                    method: 'getScript',
+                    url:url,
+                };
+                this.postMessage(messageData,handler);
+            };
 
-		// set the RogueBM object
-	self['RogueBM']=self['RogueBM'] || {}; //in block notation so closure compiler will 'export' the vairable
-	self['RogueBM'].lastCMD=function(){return cmd};
-	if(window['RogueBM']['show']){
-		//if crossorignlocal storage not loaded then load it
+            this['postMessage'] = function(messageData,handler) {
+                // var str=new Array(17);
+                // for(var i=0;i<15;i++){
+                //  var r = Math.random() * 16 | 0;
+                //  //v = c == 'x' ? r : (r & 0x3 | 0x8);
+                //  str[i]=r.toString(16);
+                // }
+                // str[15]='-'+Date.now() //put the date on the end to speed up
+                //var id=str.join('')
+
+                var id=UUID();
+
+                messageData['messageID']=id;
+                messageQueue[id]={fn:handler,method:messageData.method};
+                if(preloadQueue != null){
+                    preloadQueue.push(messageData);
+                    return;
+                }
+                xOriginElement.postMessage(JSON.stringify(messageData), '*'); //TODO fix this security risk
+            };
+        };
+        self['RogueBM']['CrossOriginLocalStorage']= CrossOriginLocalStorage;
+
+        var allowedOrigins = ['https://ktsuttlemyre.github.io'];
+        self['RogueBM']['xDLStorage'] = new CrossOriginLocalStorage(self, 'https://ktsuttlemyre.github.io/RogueBookmarklets/RogueRunner.html' , allowedOrigins);
+    }
+
+    
+
+
+
+    var NotLoadedRogueBM=!self['RogueBM'];
+
+    // pollyfill for date.now
+    if (!Date.now) {
+        Date.now = function now() {
+            return new Date().getTime();
+        };
+    }
+
+        // set the RogueBM object
+    self['RogueBM']=self['RogueBM'] || {}; //in block notation so closure compiler will 'export' the vairable
+    self['RogueBM'].lastCMD=function(){return cmd;};
+    if(window['RogueBM']['show']){
+        //if crossorignlocal storage not loaded then load it
     if(!self['RogueBM']['CrossOriginLocalStorage']){
       loadCrossOriginLocalStorage();
     }
@@ -252,43 +263,45 @@
       window['RogueBM']['show']();
     }
 
-	}
+    }
 
 
-	function injectScript(src,token,forceIframe){
-		if(forceIframe == null){
-			forceIframe=options['forceIframeInjecting'];
-		}
-		/*low level injection script. 
-		Use RogueBookmarklet.loadScript for more reliable script loading
-		*/
-		if(token != sessionID){
-			console.error('RogueRunner[injector]: sessionID either not correct or not provided. Will not load this url',src)
-			return 1
-		}
-		if(forceIframe){
-			// use this to test script injection failures to load
-			setTimeout(function(){getScriptFromLocalStorageIframe(src);},1);
-		}else{
-			appendToHead(ScriptOBJ(src,null,function(err){getScriptFromLocalStorageIframe(src,err);}));
-		}
-		return 0
-	}
+    function injectScript(src,token,forceIframe){
+        if(forceIframe == null){
+            forceIframe=options['forceIframeInjecting'];
+        }
+        /*low level injection script. 
+        Use RogueBookmarklet.loadScript for more reliable script loading
+        */
+        if(token != sessionID){
+            console.error('RogueRunner[injector]: sessionID either not correct or not provided. Will not load this url',src);
+            return 1;
+        }
+        if(forceIframe){
+            // use this to test script injection failures to load
+            setTimeout(function(){getScriptFromLocalStorageIframe(src);},1);
+        }else{
+            appendToHead(ScriptOBJ(src,null,function(err){getScriptFromLocalStorageIframe(src,err);}));
+        }
+        return 0;
+    }
 
-	//a bit of security 
-	var sessionID=UUID();
-	//inject the rogue runner dialog
-	var doc=document.documentElement;
-	var skin=options['skin'];
-	skin=( (("all" in doc.style) || ("cssall" in doc.style)) && (!!skin != false) )?'_'+skin:'';
-	var src='https://ktsuttlemyre.github.io/RogueBookmarklets/RogueRunner_src'+skin+'.js?user='+options['user']+'&cmd='+cmd;
+    //a bit of security 
+    var sessionID=UUID();
+    //inject the rogue runner dialog
+    var doc=document.documentElement;
+    var skin=options['skin'];
+    skin=( (("all" in doc.style) || ("cssall" in doc.style)) && ( !!skin != false) )?'_'+skin:'';
+    var src='https://ktsuttlemyre.github.io/RogueBookmarklets/RogueRunner_src'+skin+'.js?user='+options['user']+'&cmd='+cmd;
 
 
-	window['RogueBM']['injectScript']=injectScript //helper function for loading external scripts (//TODO maybe remove this? make it more difficult?)
-	window['RogueBM']['getSessionID']=function(){prompt('Copy the session id below to use in protected RogueBM[injector] calls',sessionID)}
-	window['RogueBM']['about']={'injector':{'revision':'{{ site.github.build_revision }}','version':vers}}
-	
-	injectScript(src,sessionID);
-	
-	loadCrossOriginLocalStorage();
+    window['RogueBM']['injectScript']=injectScript; //helper function for loading external scripts (//TODO maybe remove this? make it more difficult?)
+    window['RogueBM']['getSessionID']=function(){
+      prompt('Copy the session id below to use in protected RogueBM[injector] calls',sessionID);
+    };
+    window['RogueBM']['about']={'injector':{'revision':'{{ site.github.build_revision }}','version':vers}};
+    
+    injectScript(src,sessionID);
+    
+    loadCrossOriginLocalStorage();
 })('0.0.1',window,{'user':'anonymous','skin':'experimental','forceIframeInjecting':false} /*,cmd*/);
