8dc1a3363b8c60f2fb278173e3c5c49ec77542f0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-06 15:05:50 -0400

    Create template.js

---
+++ dev/template.js
@@ -0,0 +1,9 @@
+/**
+ *  Put your description here
+ *  You can use multi lines
+ *  Original Source: {@link ######}
+ *  @param {type} describe what you expect to see as input
+ *  @param {type} describe what you expect to see as input
+ *  @returns {type}
+ *  @author Jane Smith <jsmith@example.com> 
+ */
