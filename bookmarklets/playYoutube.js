c7d64c0234172e0042baf183502614054b7f0b5b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-07 13:15:44 -0400

    Update playYoutube.js

--- bookmarklets/playYoutube.js
+++ bookmarklets/playYoutube.js
@@ -7,39 +7,33 @@
  */
  
 (function(global){
-  if(!global.['RogueBM']['xDLStorage']){
-   alert('need xDLStorage loaded from RogueRunner to work')
-   return
- }
-  var storage=global.['RogueBM']['xDLStorage']
-    var str=prompt('What youtube object would you like to play?','')
-  
-     //Playground
-    self['RogueBM']['xDLStorage'].setData('name', 'buren')
-    var onMessage = function(payload, event) {
-     //console.log('inject got',payload,event)
-     var data = payload.data;
-     switch (payload.method) {
-         case 'get':
-             alert('message data'+ JSON.stringify(payload));
-             break;
-         default:
-             console.error('Unknown method "' + payload.method + '"', payload);
-         }
-     };
-    self['RogueBM']['xDLStorage'].getData('YoutubePlayer:status',function(payload,event){
-     if(payload){
-      storage.setItem('youtubePlayer', JSON.stringify({search:search,timestamp:new Date().getTime(),origin:window.location.href}));
-     }else{
+  var storage=null;
+  if(global['RogueBM'] && global['RogueBM']['xDLStorage']){
+    storage=global['RogueBM']['xDLStorage']
+  }else{
+    alert('need xDLStorage loaded from RogueRunner to work')
+    return
+  }
+  var str=prompt('What youtube object would you like to play?','list=PLglUu7rOaMDvrbxvSXjhCZuguPBV6YcPu')
+
+
+  storage.getData('YoutubePlayer:status',function(err,payload,event){
+    if(err){
+      console.error(err)
+      return
+    }
+    if(payload){ //if we have an instance running somewhere then use it
+      storage.setItem('youtubePlayer', JSON.stringify({command:str,timestamp:new Date().getTime(),origin:window.location.href}));
+    }else{ //create a new instance
       var externalWindowString="toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=300,top="+(screen.height-800)+",left="+(screen.width-300)
-       var externalWindow=function(url){
-         var win = window.open(url, _blank, externalWindowString);
-       }
-       externalWindow('https://ktsuttlemyre.github.io/RogueBookmarklets/site/player#?search'+search)
-     }
-    })
-
-
-
+      var externalWindow=function(url){
+        var win = window.open(url, _blank, externalWindowString);
+      }
+      if(str.indexOf('=')<=0){
+        str='?'+str
+      }
+      externalWindow('https://ktsuttlemyre.github.io/RogueBookmarklets/site/player#'+str)
+    }
+  })
 
 })(window)
