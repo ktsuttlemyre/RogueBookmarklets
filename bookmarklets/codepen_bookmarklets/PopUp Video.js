2afb1a64c4581d34e30275a7c8adfc7b79ee3c16
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-06 20:42:46 -0400

    Update PopUp Video.js

--- bookmarklets/codepen_bookmarklets/PopUp Video.js
+++ bookmarklets/codepen_bookmarklets/PopUp Video.js
@@ -4,4 +4,12 @@
  *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
  */
 
-javascript:(function(){var r = /^.*(?:vimeo\.com\/(\w{8})|(?:youtu.be\/|v\/|u\/\w\/|embed\/|v=)([^#\&\? ]*)).*/gi, m = r.exec(window.location.href); if (m[1]) { window.open("http://player.vimeo.com/video/"+m[1]); } else if (m[2]) { window.open("http://www.youtube.com/watch_popup/?v="+m[2], "_blank", "height=345,width=560"); }})()
+
+  var r = /^.*(?:vimeo\.com\/(\w{8})|(?:youtu.be\/|v\/|u\/\w\/|embed\/|v=)([^#&\? ]*)).*/gi, m = r.exec(window.location.href);
+  if (m[1]) {
+    window.open("http://player.vimeo.com/video/" + m[1]);
+  } else {
+    if (m[2]) {
+      window.open("http://www.youtube.com/watch_popup/?v=" + m[2], "_blank", "height=345,width=560");
+    }
+  }
