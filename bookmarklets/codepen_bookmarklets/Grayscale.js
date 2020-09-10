0a05be4ceb0330d78486ef3f691aa53f777260d3
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-09 22:40:18 -0400

    Update Grayscale.js

--- bookmarklets/codepen_bookmarklets/Grayscale.js
+++ bookmarklets/codepen_bookmarklets/Grayscale.js
@@ -4,4 +4,11 @@
  *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
  */
 
-javascript:void function(){(function(){function i(i){document.body.style.filter=i,document.body.style.webkitFilter=i}window.isGrayscale?(i("none"),window.isGrayscale=!1):(i("grayscale(1)"),window.isGrayscale=!0)})()}();
+void function() {
+  (function() {
+    function i(i) {
+      document.body.style.filter = i, document.body.style.webkitFilter = i;
+    }
+    window.isGrayscale ? (i("none"), window.isGrayscale = !1) : (i("grayscale(1)"), window.isGrayscale = !0);
+  })();
+}();
