f4e84419b326dbf3ec2fcf23ee6406d98e9dc6d1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-10 14:26:52 -0400

    Update and rename qrcode.js to template.js

---
+++ _scripts/template.js
@@ -0,0 +1,17 @@
+---
+description: |
+    <h1>This is some markdown content in YAML that will be output as an </h1>.
+    describe the code here
+
+    you can use multiple lines. Its totally cool here
+params: |
+    {type} describe what you expect to see as input
+    {type} describe what you expect to see as input
+returns: |
+    {bool} somethign something
+authors: |
+    Jane Doe <another url>
+    kyle suttlemyre <url>
+layout: script
+---
+alert('Aye, It\'s just me. Imma template!');
