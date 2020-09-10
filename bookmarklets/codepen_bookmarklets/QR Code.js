994db905d9a08afbaa2b4bb10a3a65d9d9930d8d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-10 10:45:40 -0400

    Update and rename QR Code.js to to-qr-code(google).js

--- bookmarklets/codepen_bookmarklets/QR Code.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file Generate a QR code for the current page
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript: (function(){window.open("http://chart.apis.google.com/chart?cht=qr&chs=500x500&chl=" + window.location,"", "width=500, height=500")})()
