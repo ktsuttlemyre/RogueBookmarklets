b737a700e07aec7ce9c3bc89d365326dc368e515
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-10 23:27:24 -0400

    Update Auto Detect Language.js

--- _scripts/Auto Detect Language.js
+++ _scripts/Auto Detect Language.js
@@ -1,12 +1,8 @@
 ---
 description: |
-    <h1>This is some markdown content in YAML that will be output as an </h1>.
-    describe the code here
-
-    you can use multiple lines. Its totally cool here
+    Uses google translate to autodetect language and convert
 params: |
-    {type} describe what you expect to see as input
-    {type} describe what you expect to see as input
+    {string} url current location or selected text
 returns: |
     {bool} something something
 authors: |
@@ -16,13 +12,11 @@ originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better
 layout: script
 ---
 {{ raw }}
-
 var t = window.getSelection && window.getSelection() || document.getSelection && document.getSelection() || document.selection && document.selection.createRange && document.selection.createRange().text;
 var e = document.charset || document.characterSet;
 if (t != "") {
-  location.href = "http://translate.google.com/translate_t?text=" + t + "&hl=en&langpair=auto|en&tbb=1&ie=" + e;
+  open("http://translate.google.com/translate_t?text=" + t + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
 } else {
-  location.href = "http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e;
-}
-;
+  open("http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
+};
 {{ endraw }}
