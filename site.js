1f074a6fb5932d9a573f3ac47bbbc4573f2affbe
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 19:44:37 -0400

    Update site.js

--- site.js
+++ site.js
@@ -4,5 +4,6 @@ var version= {{ site.github.build_revision }}
 var site={
   "site":{{ site | jsonify }},//Site wide information + configuration settings from _config.yml. See below for details.
    "page":{{ page | jsonify }},//Page specific information + the front matter. Custom variables set via the front matter will be available here. See below for details.
+  "layout":{{ layout | jsonify }},//Layout specific information + the front matter. Custom variables set via front matter in layouts will be available here.
 
 }
