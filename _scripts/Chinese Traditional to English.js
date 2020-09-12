d9adf5cf36f2c3a93253ec10cb9ac7645336afce
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 12:18:51 -0400

    Update Chinese Traditional to English.js

--- _scripts/Chinese Traditional to English.js
+++ _scripts/Chinese Traditional to English.js
@@ -16,6 +16,6 @@ originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better
 layout: script
 ---
 {{ raw }}
-
+var t=((window.getSelection&&window.getSelection())||(document.getSelection&&document.getSelection())||(document.selection&&document.selection.createRange&&document.selection.createRange().text));var e=(document.charset||document.characterSet);if(t!=''){location.href='http://translate.google.com/translate_t?text='+t+'&hl=en&langpair=zh-CN|en&tbb=1&ie='+e;}else{location.href='http://translate.google.com/translate?u='+escape(location.href)+'&hl=en&langpair=zh-CN|en&tbb=1&ie='+e;};
 
 {{ endraw }}
