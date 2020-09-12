37b0c5478e5a8f909323372e16eeead09a9115cf
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 19:39:59 -0400

    Update site.js

--- site.js
+++ site.js
@@ -2,7 +2,4 @@
 ---
 var site={
   "site":{{ site | jsonify }},//Site wide information + configuration settings from _config.yml. See below for details.
-  "page":{{ page | jsonify }},//Page specific information + the front matter. Custom variables set via the front matter will be available here. See below for details.
-  "layout":{{ layout | jsonify }},//Layout specific information + the front matter. Custom variables set via front matter in layouts will be available here.
-  "content":{{ content | jsonify }},//In layout files, the rendered content of the Post or Page being wrapped. Not defined in Post or Page files.
 }
