b56dbfdbf025325cf4f8d758da751f1af2d6d42d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 12:39:28 -0400

    Update View CSS.js

--- _scripts/View CSS.js
+++ _scripts/View CSS.js
@@ -17,5 +17,5 @@ layout: script
 ---
 {{ raw }}
 
-
+s=document.getElementsByTagName('STYLE'); ex=document.getElementsByTagName('LINK'); d=window.open().document; /*set base href*/d.open();d.close(); b=d.body; function trim(s){return s.replace(/^s*n/, '').replace(/s*$/, ''); }; function iff(a,b,c){return b?a+b+c:'';}function add(h){b.appendChild(h);} function makeTag(t){return d.createElement(t);} function makeText(tag,text){t=makeTag(tag);t.appendChild(d.createTextNode(text)); return t;} add(makeText('style', 'iframe{width:100%;height:18em;border:1px solid;')); add(makeText('h3', d.title='Style sheets in ' + location.href)); for(i=0; i<s.length; ++i) { add(makeText('h4','Inline style sheet'  + iff(' title=
 {{ endraw }}
