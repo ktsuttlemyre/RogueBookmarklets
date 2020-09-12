6ad61f06261a24164b74fee26c9b0a3fede94b31
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 12:21:19 -0400

    Update Disable Image.js

--- _scripts/Disable Image.js
+++ _scripts/Disable Image.js
@@ -16,6 +16,5 @@ originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better
 layout: script
 ---
 {{ raw }}
-
-
+javascript:function C(v)%7breturn '<td>'+v+'</td><td>'+((v>>4).toString(16)+(v&15).toString(16)).toUpperCase()+'</td><td bgcolor=DDDDDD>&'+'#'+v+';</td>%0A<p>‘;%7Dvar c=4,b=Math.ceil(224/c),a=’<table border=0>%0A<tr>‘;for(j=0;j<c;j++)%7Ba+='%0A%0A<td>DEC</td>%0A<td>HEX</td>%0A<td>ASC</td>%0A<p>‘;%7Da+=’</tr>%0A<p>‘;for(i=33;i<33+b;i++)%7Ba+='%0A%0A<tr>‘;for(j=0;j<c;j++)%7Bt=i+(j*b);if(t<=255)a+=C(t);%7Da+='</tr>%0A<p>‘;%7Da+=’</table>%0A<p>‘;var W=open(”,”,’width=500,height=600,left=0,top=0,resizable,scrollbars’);void(W.document.write(a));”><strong>ASCII Table</strong></a> – Brings up a ASCII table for your reference.</p>%0A<p><a class=
 {{ endraw }}
