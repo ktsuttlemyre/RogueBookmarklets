03c87292f2e962fb9051d423a0c32f521c73ff4e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 00:37:39 -0400

    Update GoogleTranslate.js

--- _scripts/GoogleTranslate.js
+++ _scripts/GoogleTranslate.js
@@ -1,21 +1,28 @@
 ---
 description: |
-    Use Google Translate to automatically detect the language and convert it to your own native language
+    This bookmarklet attempts to detect your native language and then provide a convinent way to translate words and phrases.
+    If you highlight a word or phrase on any webpage and then run this command it will translate the highlighted word for you.
+    If no highlited words are selected this command will use Google Translate to automatically detect the page language and convert it to your own native language
 params: |
-    {string} url current location or selected text
+    {string} highlighted phrase, url or current location
 returns: |
     {bool} something something
 authors: |
-    Hong Kiat collection <hongkiat.com>
     Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
 originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better-productivity-ultimate-list/
 layout: script
 ---
 {{ raw }}
+
+// var trans = window.getSelection();
+// if(trans){
+//     return window.open("https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=" + trans);
+// }
+
 var e = document.charset || document.characterSet;
-if (selection != "") {
-  open("http://translate.google.com/translate_t?text=" + selection + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
+if (window.getSelection() != "") {
+  window.open("http://translate.google.com/translate_t?text=" + selection + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
 } else {
-  open("http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
+  window.open("http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
 };
 {{ endraw }}
