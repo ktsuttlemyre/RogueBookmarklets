a7baa8fbf3b7b1113582bf41b9f4480400253732
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 00:48:09 -0400

    Delete Inject StyleSheet.js

--- bookmarklets/codepen_bookmarklets/Inject StyleSheet.js
+++
@@ -1,8 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file Inject CSS into site<br><i class="gray">Simply replace the CSS/URL in this script with the StyleSheet you want to use.</i>
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript:(function(){var s=document.createElement('link');s.setAttribute('href','http://l.yimg.com/a/lib/arc/core_1.0.5.css');
-	s.setAttribute('rel','stylesheet');s.setAttribute('type','text/css');document.getElementsByTagName('head')[0].appendChild(s);alert('Stylesheet injected!');})();
