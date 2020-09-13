e307e3121a6ede7e70ca5e7661ff954be6fab7a2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 19:05:30 -0400

    Create toModal.js

---
+++ _scripts/toModal.js
@@ -0,0 +1,17 @@
+---
+description: |
+    <h1>This is some markdown content in YAML that will be output as a header </h1>.
+    describe the code here
+
+    you can use multiple lines. Its totally cool here
+params: |
+    {type} VarName - describe what you expect to see as input
+    {type} VarName - describe what you expect to see as input
+returns: |
+    {bool} Something something
+authors: |
+    Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+originalsource: codepen.com/something/something
+layout: script
+---
+alert('//TODO Implement this')
