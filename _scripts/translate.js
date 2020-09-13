b7ed01cdaf9abf72f36b2fdfe4f1f37238f63f2d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 16:52:11 -0400

    Update translate.js

--- _scripts/translate.js
+++ _scripts/translate.js
@@ -23,7 +23,7 @@ var e = document.charset || document.characterSet;
 var selection=window.getSelection();
 debugger
 if (selection) {
-  window.open("http://translate.google.com/translate_t?text=" + selection + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
+  location.href=("http://translate.google.com/translate_t?text=" + selection + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
 } else {
-  window.open("http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
+  location.href=("http://translate.google.com/translate?u=" + escape(location.href) + "&hl=en&langpair=auto|en&tbb=1&ie=" + e);
 }
