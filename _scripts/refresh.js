6200a7281c1cb785e7135c266fb64d1f0d647f3a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 15:36:56 -0400

    Create refresh.js

---
+++ _scripts/refresh.js
@@ -0,0 +1,14 @@
+---
+description: |
+    Simmple. Just refreshes the page!
+params: |
+    {undefined} nothing
+returns: |
+    {undefined} nothing cause we gonna refresh
+authors: |
+    kyle suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets/>
+layout: script
+---
+window.location.reload && window.location.reload();
+window.location.href=window.location.href;
+window.location=window.location;
