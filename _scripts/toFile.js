218dd8ea25135efce5f6946a86b805cef54cca6d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 19:07:51 -0400

    Create toFile.js

---
+++ _scripts/toFile.js
@@ -0,0 +1,12 @@
+---
+description: |
+    This is a set of toCommands. It will take selected text, RogueBM.stdin or prompt the user for input to act on
+params: |
+    {string} input - value to act upon
+returns: |
+    {undefined} Should do an action
+authors: |
+    Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+layout: script
+---
+alert('//TODO Implement this')
