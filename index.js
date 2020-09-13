437eed229111da21f089aea44850eb90a4ab3879
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-12 20:55:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ window.RogueBM.about.scripts={
 //        }
 window.RogueBM.scripts={
   {%- assign allScripts = '' | split: '' -%}
+  {%- allScripts | pop -%}
 
   {%- for coll in site.collections -%}
   {%- unless coll.label == "posts" -%}
