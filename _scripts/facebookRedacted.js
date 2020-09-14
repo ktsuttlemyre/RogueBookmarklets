68e9b10fae9db40c284e9903c542c206202e9137
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-13 21:37:15 -0400

    Update facebookRedacted.js

--- _scripts/facebookRedacted.js
+++ _scripts/facebookRedacted.js
@@ -1,4 +1,13 @@
 ---
+description: |
+    This is a set of toCommands. It will take selected text, RogueBM.stdin or prompt the user for input to act on
+params: |
+    {string} input - value to act upon
+returns: |
+    {undefined} Should do an action
+authors: |
+    Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+layout: script
 ---
 (function(aliasDatabase,global){
 	var seed=9834579834890
