3a69e3ea756b9c1a08c77d038c202ede8e5c0103
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-15 00:31:59 -0400

    Delete scrape-media-from-page.js

--- bookmarklets/codepen_bookmarklets/scrape-media-from-page.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file 
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript:(function(){window.open('http://savefrom.net/?url='+encodeURIComponent(location.href)+'&utm_source=bookmarklet&utm_medium=bookmarklet&utm_campaign=bookmarklet');})()
