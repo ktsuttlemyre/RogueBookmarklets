0df9aeb0e74619b5c0179c7efdd69bc1f7709c12
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-17 19:05:05 -0400

    Update template.js

--- dev/template.js
+++ dev/template.js
@@ -1,14 +1,14 @@
 ---
 description: |
-    <h1>This is some markdown content in YAML that will be output as a header </h1>.
-    describe the code here
+  <h1>This is some markdown content in YAML that will be output as a header </h1>.
+  describe the code here
 
-    you can use multiple lines. Its totally cool here
+  you can use multiple lines. Its totally cool here
 
 ####### Other Metadata #######
-authors: |
-    Jane Doe <another url>
-    Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+authors:
+- Jane Doe <another url>
+- Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
 originalsource: codepen.com/something/something
 
 
@@ -18,11 +18,11 @@ specs: async textselect stdin stdout
 layout: script
 
 # for syntax see: https://jsdoc.app/tags-type.html
-params: |
-    {type} VarName - describe what you expect to see as input
-    {type} VarName - describe what you expect to see as input
-returns: |
-    {bool} Something something
+params:
+- {type} VarName - describe what you expect to see as input
+- {type} VarName - describe what you expect to see as input
+returns:
+- {bool} Something something
 ---
 
 var aLiquidVariable = {{ site.github.build_revision }}
