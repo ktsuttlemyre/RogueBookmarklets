8ccc8ea0c44cd629d71554fd96a90470091477c2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-17 20:07:15 -0400

    Update closure-compiler.js

--- bookmarklets/closure-compiler.js
+++ bookmarklets/closure-compiler.js
@@ -1,3 +1,20 @@
+---
+description: |
+    <h1>This is some markdown content in YAML that will be output as an </h1>.
+    describe the code here
+
+    you can use multiple lines. Its totally cool here
+params: |
+    {type} describe what you expect to see as input
+    {type} describe what you expect to see as input
+returns: |
+    {bool} something something
+authors: |
+    Hong Kiat collection <hongkiat.com>
+    Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better-productivity-ultimate-list/
+layout: script
+---
 /*
 Closure compiler api which exposes a function with the signature
 code = string (can be 'clear_cache' to clear cache)
