31ac6a5087c415a0e98a5de02e85bbff219ccefe
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-26 18:11:48 -0400

    Update template.js

--- dev/template.js
+++ dev/template.js
@@ -6,25 +6,24 @@ description: |
   you can use multiple lines. Its totally cool here
 
 ####### Other Metadata #######
-authors:
-- Jane Doe <another url>
-- Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+authors: |
+  Jane Doe <another url>
+  Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
 originalsource: codepen.com/something/something
 
 
 
 ####### function signature #######
-specs: async textselect stdin stdout
+specs: [async, textselect, stdin, stdout]
 layout: script
 
 # for syntax see: https://jsdoc.app/tags-type.html
-params:
-- {type} VarName - describe what you expect to see as input
-- {type} VarName - describe what you expect to see as input
-returns:
-- {bool} Something something
+params: |
+  {type} VarName - describe what you expect to see as input
+  {type} VarName - describe what you expect to see as input
+returns: |
+  {bool} Something something
 ---
-
 var aLiquidVariable = {{ site.github.build_revision }}
 var g={g:function(){alert('Aye, It\'s just me. Imma template!');}}
 g.g()
