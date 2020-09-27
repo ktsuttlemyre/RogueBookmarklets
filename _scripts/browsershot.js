853b9ebf33c10422d63534d67d36bc10b4e0954f
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-27 19:32:38 -0400

    Update and rename Browsershot.js to browsershot.js

--- _scripts/browsershot.js
+++ _scripts/browsershot.js
@@ -16,5 +16,5 @@ originalsource: https://www.hongkiat.com/blog/100-useful-bookmarklets-for-better
 layout: script
 ---
 {{ raw }}
-void(location='http://browsershots.org/?url='+escape(location))
+void(location.href='http://browsershots.org/?url='+encodeURIComponent(location))
 {{ endraw }}
