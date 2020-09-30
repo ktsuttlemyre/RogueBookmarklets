530f1e1fb5b7d6ff9a1941cf2a1b55d5a6246f5c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-30 17:14:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ window.RogueBM.scripts={
     {%- assign path = script.url | default: script.path -%}
     {%- assign path = path | split: "/" | compact | join: "/" -%}
     {%- assign name = script.name | split: "." | first | escape  -%}
-      {%- if path contains 'bookmarklets' -%}
+      {%- if path contains "bookmarklets" -%}
        "{{ name }}":{
-         test {{path | split "/" | shift | first}}
           "name":"{{ name }}",
           "path":"{{ path }}",
           "index":{% increment counter %},
