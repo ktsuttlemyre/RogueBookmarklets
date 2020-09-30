19f06540b8a794b943f09157ebfbe6f805575569
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-09-30 14:57:23 -0400

    Update template.js

--- dev/template.js
+++ dev/template.js
@@ -17,6 +17,8 @@ originalsource: codepen.com/something/something
 specs: [async, textselect, stdin, stdout]
 layout: script
 data-privacy: [local-only, third-party, single-party]
+libraries:
+  - ["https://code.jquery.com/jquery-3.5.1.min.js", "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" ]
 
 # for syntax see: https://jsdoc.app/tags-type.html
 params: |
