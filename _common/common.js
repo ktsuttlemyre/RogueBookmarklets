a12d08f487be4342930f0abf7e2bd9e2bbf70026
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-03 16:17:17 -0400

    Update common.js

--- _common/common.js
+++ _common/common.js
@@ -82,3 +82,26 @@ function getCommentBlock(text) {
             return element;
         }
     })()
+
+    
+    (function(){
+  //encodeURI(RogueBM.stringFormat(RogueBM.scriptEndpoints.edit,RogueBM.scripts['to_qr']))
+  var format=function(prefix,suffix){
+    return  function(str){
+      "use strict";
+       // this is the stringFormat function used at stackoverflow
+       // orignal https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+       var t = typeof arguments[1];
+       var args = (t === "object" || (arguments[1].length!=null && t!=="string")) ?
+            arguments[1]
+           : Array.prototype.slice.call(arguments,1);
+       var keys=Object.keys(args)
+       for (var i=0,l=keys.length;i<l;i++) {
+           str = str.replace(new RegExp(prefix + keys[i] + suffix, "gi"), args[keys[i]]);
+       }
+       return str;
+    };
+  }
+  RogueBM.stringFormat=format("\\{","\\}")
+  RogueBM.stringHandlebarFormat=format("\\{\\{","\\}\\}")
+})();
