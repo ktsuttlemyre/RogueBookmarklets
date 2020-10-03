a047db3da4bee2f8e1ed0cfe6174eea83af40dd5
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-02 23:32:40 -0400

    Update aardvark.js

--- _scripts/aardvark.js
+++ _scripts/aardvark.js
@@ -4,12 +4,26 @@ description: |
     - Clean up unwanted banners and surrounding "fluff," especially prior to printing a page
     - See how the page is created, block by block
     - View the source code of one or more elements
+deprecated: Originally created in 2005 it doesn't seem to work as well at it once did as browsers have evovled a lot since then.
+
+####### Other Metadata #######
 authors: |
     Rob Brown <rob@karmatics.com>
 originalsource: https://www.karmatics.com/aardvark/
-deprecated: Originally created in 2005 it doesn't seem to work as well at it once did as browsers have evovled a lot since then.
+
+####### function signature #######
+qualified urls: []
+specs: [async]
 layout: script
+type: tool
+data privacy: [ third-party-inline]
+libraries:
+  - ["http://www.karmatics.com/aardvark/loader.js", "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" ]
+
+# for syntax see: https://jsdoc.app/tags-type.html
+params: 
+returns:
 ---
 {{ raw }}
-document.getElementsByTagName("head")[0].appendChild(document.createElement("script")).setAttribute("src", "http://www.karmatics.com/aardvark/loader.js");
+
 {{ endraw }}
