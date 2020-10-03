4b6e9bbae05c1dcdb7056d20fc1f07e3ce0db494
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-03 01:40:29 -0400

    Update alexa.js

--- _scripts/alexa.js
+++ _scripts/alexa.js
@@ -2,14 +2,26 @@
 description: |
     hows you the Alexa ranking of the site.
     SEO and Competitive analyhsis software created by amazon.com
-params: |
-    {string} current location
-returns: |
-    {null} opens alexa window with current url as search
+  
+#deprecated: 
+####### Other Metadata #######
 authors: |
     Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
+originalsource: codepen.com/something/something
+
+####### function signature #######
+qualified urls: []
+specs: []
 layout: script
+type: tool
+data privacy: [third-party-visit]
+
+# for syntax see: https://jsdoc.app/tags-type.html
+params: |
+  {string} location=stdin|location.hostname - current location
+returns: |
+  {undefined} opens alexa window with current url as search
 ---
 {{ raw }}
-open("https://www.alexa.com/siteinfo/" + encodeURIComponent(location.hostname));
+open("https://www.alexa.com/siteinfo/" + encodeURIComponent(location));
 {{ endraw }}
