6b1014239d119ed4b7ae4b3d625cfb3cba4e1c3d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-03 01:34:44 -0400

    Update acronym-finder.js

--- _scripts/acronym-finder.js
+++ _scripts/acronym-finder.js
@@ -1,17 +1,30 @@
 ---
 description: |
     With more than 1,000,000 human-edited definitions, Acronym Finder is the world's largest and most comprehensive dictionary of acronyms, abbreviations, and initialisms. Combined with the Acronym Attic, Acronym Finder contains more than 5 million acronyms and abbreviations. You can also search for more than 850,000 US and Canadian postal codes.
+  
+####### Other Metadata #######
 authors: |
     Kyle Suttlemyre <https://github.com/ktsuttlemyre/RogueBookmarklets>
 originalsource: https://www.acronymfinder.com/
+
+####### function signature #######
+qualified urls: []
+specs: []
 layout: script
+type: tool
+data privacy: [third-party-visit]
+
+# for syntax see: https://jsdoc.app/tags-type.html
+params: |
+  {string} word=getSelection - word to find the acronym for
+returns: |
+  {undefined} 
 ---
 {{ raw }}
-var Qr = document.getSelection().toString();
-if (!Qr) {
-  Qr = prompt("Input an acronym", "");
+if (!word) {
+  word = prompt("Input an acronym", "");
 }
-if (Qr) {
-  open("http://www.acronymfinder.com/af-query.asp?String=exact&Acronym=" + escape(Qr) + "&Find=Find");
+if (word) {
+  open("http://www.acronymfinder.com/af-query.asp?String=exact&Acronym=" + encodeURIComponent(word) + "&Find=Find");
 };
 {{ endraw }}
