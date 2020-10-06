bbcb2ec0c5ac1fbfe8c65200a169d77935d8d20c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-06 15:38:13 -0400

    Create whatfont.js

---
+++ _scripts/whatfont.js
@@ -0,0 +1,2 @@
+//http://www.chengyinliu.com/whatfont.html
+javascript:(function(){var d=document,s=d.createElement('scr'+'ipt'),b=d.body,l=d.location;s.setAttribute('src','http://chengyinliu.com/wf.js?o='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));b.appendChild(s)})();
