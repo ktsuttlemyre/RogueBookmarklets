43d176fcceeefe9f0d366b80655c8fb47082b4ce
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-08 17:37:25 -0400

    Update bind.js

--- _scripts/bind.js
+++ _scripts/bind.js
@@ -28,33 +28,39 @@ if(!script){
     profile='.*';
 }
 var key='@';
+var callback=function(err,data){
+  if(err){
+    console.error(err);
+  }
+  next(data);
+}
 method=method.toLowerCase();
 switch(method){
     case 'push':
     case 'shift':
-            getData(key,function(data){
+            getData(key,function(err,data){
                 data=data||{};
                 var array=data[profile]||[];
                 array[method](script);
-                setData(key,data,next);
+                setData(key,data,callback);
             });
         break;
     case 'set':
-            getData(key,function(data){
+            getData(key,function(err,data){
                 data=data||{};
                 data[profile]=script;
-                setData(key,data,next);
+                setData(key,data,callback);
             });
         break;
     case 'get':
-            getData(key,next);
+            getData(key,callback);
         break;
     case 'remove':
-            getData(key,function(data){
+            getData(key,function(err,data){
                 data=data||{};
                 data[profile]=null;
                 delete data[profile];
-                setData(ns,data,next);
+                setData(ns,data,callback);
             });
         break;
     default:
