304100ba713885e6f3963be5b79f52bd96b817e6
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-10 15:25:03 -0400

    Rename tuneIn-mp3-grab.js to scrape-tuneIn-mp3.js

--- bookmarklets/codepen_bookmarklets/tuneIn-mp3-grab.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file Grab the MP3 from a TuneIn radio station
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript:(function(){try {var data = JSON.parse(document.getElementsByTagName("PRE")[0].innerHTML);data.Streams.forEach(function(element) {prompt(element.Badwidth + "kbps: ", element.Url);});}catch(err) {alert = "This is not a valid TuneIn metadata page."}})()
