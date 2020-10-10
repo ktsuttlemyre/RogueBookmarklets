262bc30377ae4a83e22ba57aaff47413522391c8
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-10 15:21:04 -0400

    Delete _Google Cache.js

--- bookmarklets/codepen_bookmarklets/_Google Cache.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file Find a cached version of the current page.<br>    <i class="gray">Looking for a cached version of a page? Try this.</i>
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript:window.location = 'https://webcache.googleusercontent.com/search?q=cache:'+window.location;
