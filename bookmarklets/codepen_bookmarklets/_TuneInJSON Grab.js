52a6771cdf1872dae4a8a5d24655aa38b923ca22
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-10 15:23:11 -0400

    Rename _TuneInJSON Grab.js to tunein-json-scraper.js

--- bookmarklets/codepen_bookmarklets/_TuneInJSON Grab.js
+++
@@ -1,7 +0,0 @@
-/**
- *  @author https://codepen.io/bookmarklets
- *  @file <h3>THIS ONE NEEDS WORK - QUIRKY!</h3> Use the first button to get the JSON page, <br>the second button grabs the MP3 stream.<br><i class="gray">Grab an MP3 stream from any TuneIn station.</i>
- *  Original Source {@link https://cdpn.io/bookmarklets/fullpage/NobJbq#}
- */
-
-javascript:(function(){var url = "";var r = false;try{url = TuneIn.payload.Program.broadcast.StreamUrl;r = confirm("Station JSON URL successfully retrieved. Opening now.");if(r) {window.open(url);}}catch(e) {try {url = TuneIn.payload.Program.broadcast.StreamUrl;r = confirm("Station JSON URL successfully retrieved. Opening now.");if(r) {window.open(url);}}catch(ee) {alert("No TuneIn Station Detected");}}})()
