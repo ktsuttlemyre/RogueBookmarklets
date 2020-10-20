b44e4155f8acb40bde569b739ec0e7dfc6101edd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-19 22:19:28 -0400

    Update if.js

--- _scripts/if.js
+++ _scripts/if.js
@@ -1,20 +1,18 @@
 
 
-var index=0
-function doIteration(){
-  RogueBM.run(arguments[index],function(data){
-    index+=1
+
+function doIteration(index){
+  RogueBM.run(arguments[index++],function(data){
     if(data.data){
       RogueBM.run(arguments[index],next)
     }else{
-      index+=1
-      if(arguments.length-1<=index){
+      if(arguments.length-1<=++index){
         RogueBM.run(arguments[index],next)
       }else{
-        doIteration();
+        doIteration(++index);
       }
-  })
+    }})
 }
-doIteration();
+doIteration(0);
 
 
