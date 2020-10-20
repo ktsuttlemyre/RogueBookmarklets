9edc2640162a837720e560b0ec8972fba4d73b25
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-20 14:47:53 -0400

    Create sticky_note.js

---
+++ _scripts/sticky_note.js
@@ -0,0 +1,5 @@
+//TODO small indicator that embeds into an element. when you click on a child we search up to find a parent node with an ID and attach to that (if in a table)
+//if no id is found in the parents then look at chldren and attach to an id there
+// if no id is found either direction then attach to a tr or li
+
+//maybe use title or alt text on a link or image?
