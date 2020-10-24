8e50e32a5d140c6bd2f325c88c53866d70db5720
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2020-10-24 18:36:43 -0400

    Create replace.js

---
+++ _scripts/replace.js
@@ -0,0 +1,47 @@
+//TODO use stdin.selection as the range
+//stackoverflow.com/questions/5393922/javascript-replace-selection-all-browsers
+    function replaceSelection(html, selectInserted) {
+    var sel, range, fragment;
+
+    if (typeof window.getSelection != "undefined") {
+        // IE 9 and other non-IE browsers
+        sel = window.getSelection();
+
+        // Test that the Selection object contains at least one Range
+        if (sel.getRangeAt && sel.rangeCount) {
+            // Get the first Range (only Firefox supports more than one)
+            range = window.getSelection().getRangeAt(0);
+            range.deleteContents();
+
+            // Create a DocumentFragment to insert and populate it with HTML
+            // Need to test for the existence of range.createContextualFragment
+            // because it's non-standard and IE 9 does not support it
+            if (range.createContextualFragment) {
+                fragment = range.createContextualFragment(html);
+            } else {
+                // In IE 9 we need to use innerHTML of a temporary element
+                var div = document.createElement("div"), child;
+                div.innerHTML = html;
+                fragment = document.createDocumentFragment();
+                while ( (child = div.firstChild) ) {
+                    fragment.appendChild(child);
+                }
+            }
+            var firstInsertedNode = fragment.firstChild;
+            var lastInsertedNode = fragment.lastChild;
+            range.insertNode(fragment);
+            if (selectInserted) {
+                if (firstInsertedNode) {
+                    range.setStartBefore(firstInsertedNode);
+                    range.setEndAfter(lastInsertedNode);
+                }
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        }
+    } else if (document.selection && document.selection.type != "Control") {
+        // IE 8 and below
+        range = document.selection.createRange();
+        range.pasteHTML(html);
+    }
+}
